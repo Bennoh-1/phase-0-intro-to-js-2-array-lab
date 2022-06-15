7a828c6ba3f696238a9eb6e309912bbd003014d7
Bennoh-1 <franciskibira017@gmail.com>
2022-06-15 23:37:22 +0300

    new message

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 // Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"];
 const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];    
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat() {
+    return [...cats].slice(0, length-1);
+}
+
+function removeFirstCat() {
+    return [...cats].slice(1, cats.length);
+}
